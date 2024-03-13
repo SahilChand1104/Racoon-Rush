6eb12cf93ba00648d002ba67ae2decbe0bfe309b
Jonathan Peters <jmp22@sfu.ca>
2024-03-12 23:14:59 -0700

    Created Collectible class (only used for donuts currently)

---
+++ src/main/java/RacoonRush/entity/Collectible.java
@@ -0,0 +1,33 @@
+package RacoonRush.entity;
+
+import RacoonRush.game.Config;
+import RacoonRush.game.GamePanel;
+import RacoonRush.game.ImageLoader;
+import RacoonRush.game.Move;
+import RacoonRush.map.Tile;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Collectible extends Entity{
+
+    private final GamePanel gp;
+    private final Config config;
+    public Collectible(GamePanel gp, int x, int y) {
+        this.config = gp.getConfig();
+        this.gp = gp;
+        this.worldX = x;
+        this.worldY = y;
+        this.speed = 0;
+        this.dir = Move.DOWN;
+        this.hitbox = new Rectangle(x, y, 32, 32);
+    }
+
+    public void draw(Graphics2D g2, int x, int y, int animationFrame) {
+        ImageLoader imageLoader = gp.getImageLoader();
+        BufferedImage image = imageLoader.getDonutImage(animationFrame);
+        g2.drawImage(image, x, y, config.tileSize(), config.tileSize(), null);
+    }
+
+
+}
