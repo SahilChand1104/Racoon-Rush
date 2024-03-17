2c398d2c14e72b99201e8d4c29f97809edee42ad
Sahil Chand <Sahil.Chand1104@gmail.com>
2024-03-17 14:39:54 -0700

    Added Sound Effects in

--- src/main/java/RacoonRush/map/tile/Item.java
+++ src/main/java/RacoonRush/map/tile/Item.java
@@ -1,26 +1,40 @@
 package RacoonRush.map.tile;
 
 import RacoonRush.entity.Player;
+import RacoonRush.game.GamePanel;
 
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class Item extends Tile {
     private final int score;
+
+    private final GamePanel gamePanel;
     private boolean collected;
 
-    public Item(ArrayList<BufferedImage> images, int score) {
+    public Item( ArrayList<BufferedImage> images, int score, GamePanel gamePanel) {
         super(images);
+        this.gamePanel = gamePanel;
         this.score = score;
         collected = false;
     }
 
     @Override
     public boolean onCollide(Player player) {
-        collected = true;
-        player.updateScore(score);
+        if (!collected) {
+            collected = true;
+            player.updateScore(score);
+
+            if(score  == 10){
+                gamePanel.PlaySoundEffect(1);
+            }
+            else if(score == -20){
+                gamePanel.PlaySoundEffect(2);
+            }
+        }
         return true;
     }
+
 
     @Override
     public BufferedImage getImage(int x, int y, int animationFrame) {
