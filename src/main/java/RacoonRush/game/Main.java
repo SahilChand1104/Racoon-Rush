fe264ae2405ed974db2dc6e98c5e9ec5dc9f2038
Jonathan Peters <jmp22@sfu.ca>
2024-03-28 15:10:59 -0700

    Added taskbar icon

--- src/main/java/RacoonRush/game/Main.java
+++ src/main/java/RacoonRush/game/Main.java
@@ -2,13 +2,24 @@ package RacoonRush.game;
 
 
 import javax.swing.*;
+import javax.tools.Tool;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 /**
  * Main class for the game
  */
 public class Main {
+
     public static void main(String[] args) {
+        Taskbar taskbar = Taskbar.getTaskbar();
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Image icon_img = toolkit.getImage("src/main/resources/menu/icon.png");
+        try {
+            taskbar.setIconImage(icon_img);
+        } catch (UnsupportedOperationException e) {
+            System.out.println("The os does not support setting the icon for the taskbar");
+        }
         // Make a new window
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
